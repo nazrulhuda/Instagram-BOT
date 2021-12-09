49adffecb2ddf1c7809a8113dd1d38811609b2b8
Nazrul Huda Shanto <md.nazrul.huda.shanto@g.bracu.ac.bd>
2021-12-10 00:39:03 +0600

    asdasd

--- users/models.py
+++ users/models.py
@@ -11,12 +11,4 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+  
